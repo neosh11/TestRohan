f05858e28db2725a3bc9418dc3d6f0f9ee53f24d
Neosh <neosh11@icloud.com>
2021-10-23 02:32:08 +0800

    BAC

---
+++ components/CustomGridList/LineChart.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+import { IPoint } from '.'
+
+const useD3 = (renderChartFn: (svg: any) => void, dependencies: React.DependencyList | undefined) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    renderChartFn(d3.select(ref.current))
+    return () => {
+      return
+    }
+  }, [renderChartFn, dependencies])
+  return ref
+}
+
+interface Props {
+  data: IPoint[]
+}
+
+const DrawChart = ({ data }: Props) => {
+  interface IPoint {
+    x: number
+    y: number
+  }
+  type TData = IPoint[]
+
+  const yAccessor = (d: IPoint) => d.y
+  const xAccessor = (d: IPoint) => d.x
+
+  //   const wrapper = d3.select('#wrapper')
+  //   const svg = wrapper.append('svg')
+
+  const dimensions = {
+    margin: {
+      top: 20,
+      right: 20,
+      bottom: 50,
+      left: 50
+    }
+  }
+
+  const ref = useD3(
+    (svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>) => {
+      const width = svg.node()?.getBoundingClientRect().width
+      const height = svg.node()?.getBoundingClientRect().height
+      if (!width || !height) return
+
+      const boundedWidth = width - dimensions.margin.left - dimensions.margin.right
+      const boundedHeight = height - dimensions.margin.top - dimensions.margin.bottom
+
+      svg.attr('width', width).attr('height', height)
+
+      const yScale = d3.scaleLinear().domain(d3.extent(data, yAccessor)).range([boundedHeight, 0]).nice()
+      const xScale = d3.scaleLinear().domain(d3.extent(data, xAccessor)).range([0, boundedWidth]).nice()
+
+      const lineGenerator = d3
+        .line<IPoint>()
+        .x(d => xScale(d.x))
+        .y(d => yScale(d.y))
+
+      const line = svg
+        .select('.bounds')
+        .append('path')
+        .attr('d', lineGenerator(data))
+        .attr('fill', 'none')
+        .attr('stroke', '#af9358')
+        .attr('stroke-width', 2)
+
+      const yAxisGenerator = d3.axisLeft(yScale)
+      const yAxis = svg.select('.bounds').append('g').call(yAxisGenerator)
+
+      const xAxisGenerator = d3.axisBottom(xScale)
+      const xAxis = svg.select('.bounds').append('g').call(xAxisGenerator)
+    },
+    [data.length, dimensions]
+  )
+
+  return (
+    <svg
+      ref={ref}
+      style={{
+        height: 500,
+        width: '100%',
+        marginRight: '0px',
+        marginLeft: '0px'
+      }}
+    >
+      <g
+        className='bounds'
+        style={{
+          transform: `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+        }}
+      />
+    </svg>
+  )
+}
+
+const LineChart = ({ data }: Props) => {
+  return (
+    <>
+      <h2>Line Chart</h2>
+      <DrawChart data={data} />
+    </>
+  )
+}
+
+export default LineChart
--- components/CustomGridList/index.tsx
+++ components/CustomGridList/index.tsx
@@ -3,8 +3,9 @@ import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
 import { Drink } from 'interfaces/drinks'
 
 import Image from 'next/image'
-import { Dispatch, SetStateAction, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import Modal from '../Modal'
+import LineChart from './LineChart'
 import ListHeader from './ListHeader'
 import SelectedModal from './SelectedModal'
 
@@ -72,14 +73,40 @@ const RemoveDrink = (
   })
 }
 
+const DrankDrink = (
+  selectedDrinkName: string,
+  drankDrinksState: IDrankDrinks,
+  setDrankDrinks: Dispatch<SetStateAction<IDrankDrinks>>
+) => {
+  const newDrink: DrinkTime = {
+    name: selectedDrinkName,
+    time: new Date()
+  }
+  drankDrinksState.drankDrinks.push(newDrink)
+  console.log(drankDrinksState)
+
+  // Store locally incase refresh
+  localStorage.setItem('drankDrinks', JSON.stringify(drankDrinksState.drankDrinks))
+
+  setDrankDrinks({
+    drankDrinks: drankDrinksState.drankDrinks,
+    update: drankDrinksState.update === 0 ? 1 : 0
+  })
+}
+
 const ListItem = ({
   drink,
   selectedDrinks,
-  setSelectedDrinks
+  setSelectedDrinks,
+  drankDrinksState,
+  setDrankDrinks
 }: {
   drink: Drink
   selectedDrinks: ISelectDrinks
   setSelectedDrinks: Dispatch<SetStateAction<ISelectDrinks>>
+
+  drankDrinksState: IDrankDrinks
+  setDrankDrinks: Dispatch<SetStateAction<IDrankDrinks>>
 }) => {
   return (
     <div key={drink.name} className='col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 '>
@@ -111,7 +138,12 @@ const ListItem = ({
             />
           </div>
           <div className='-ml-px w-0 flex-1 flex'>
-            <Button text='Drank' />
+            <Button
+              text='Drank'
+              onClick={() => {
+                DrankDrink(drink.name, drankDrinksState, setDrankDrinks)
+              }}
+            />
           </div>
         </div>
       </div>
@@ -124,6 +156,101 @@ export interface ISelectDrinks {
   update: number
 }
 
+export interface DrinkTime {
+  name: string
+  time: Date
+}
+export interface IDrankDrinks {
+  drankDrinks: DrinkTime[]
+  update: number
+}
+
+const getSTDDrink = (name: string) => {
+  return 1.0
+}
+
+export interface IPoint {
+  x: number
+  y: number
+}
+type TData = IPoint[]
+
+type genderCoeff = 0.55 | 0.68
+//! Temporary
+const CalculateBAC = (
+  drankDrinksState: IDrankDrinks,
+  coeff: genderCoeff,
+  weight: number
+): { BAC: number; BACData: TData } => {
+  const decayRate = 0.015
+  const drankDrinks = drankDrinksState.drankDrinks
+
+  let BAC = 0.0
+
+  const data: TData = []
+
+  const preFirstDrink: Date = drankDrinks[0].time
+  const firstDrink: Date = typeof preFirstDrink === 'string' ? new Date(preFirstDrink) : preFirstDrink
+
+  let lastTime: Date = new Date()
+  const currentTime = new Date()
+
+  for (let i = 0; i < drankDrinks.length; i++) {
+    const tt = drankDrinks[i].time
+    const t: Date = typeof tt === 'string' ? new Date(tt) : tt
+    const std = getSTDDrink(drankDrinks[i].name)
+
+    if (i === 0) {
+      BAC += std / (weight * coeff)
+      data.push({ x: 0, y: 0 })
+      data.push({ x: 0, y: BAC })
+    } else {
+      // All other points
+
+      const elapsedTime = (t.getTime() - lastTime.getTime()) / (1000 * 3600)
+      console.log(elapsedTime)
+      let decayedBAC = BAC - decayRate * elapsedTime
+      decayedBAC = decayedBAC < 0 ? 0 : decayedBAC
+
+      const elapsedTimeFirstDrink = (t.getTime() - firstDrink.getTime()) / (1000 * 3600)
+      data.push({ x: elapsedTimeFirstDrink, y: decayedBAC })
+
+      console.log(`${BAC} dada1`)
+      console.log(`${decayedBAC} dada2`)
+
+      const addSTD = std / (weight * coeff)
+
+      BAC = decayedBAC < 0 ? addSTD : decayedBAC + addSTD
+
+      data.push({ x: elapsedTimeFirstDrink, y: BAC })
+    }
+    lastTime = t
+  }
+
+  const timeToZero = BAC / decayRate
+
+  const elapsedTimeFirstDrinkLast =
+    (lastTime.getTime() + timeToZero * 1000 * 3600 - firstDrink.getTime()) / (1000 * 3600)
+
+  const elapsedTime = (currentTime.getTime() - lastTime.getTime()) / (1000 * 3600)
+
+  if (elapsedTimeFirstDrinkLast < elapsedTime) {
+    data.push({ x: elapsedTimeFirstDrinkLast, y: 0 })
+  }
+
+  const decayedBAC = BAC - decayRate * elapsedTime
+  BAC = decayedBAC < 0 ? 0 : decayedBAC
+  const elapsedTimeFirstDrink = (currentTime.getTime() - firstDrink.getTime()) / (1000 * 3600)
+  data.push({ x: elapsedTimeFirstDrink, y: BAC })
+
+  return {
+    BAC: BAC,
+    BACData: data
+  }
+
+  // Assume every drink 1 std drank
+}
+
 export default function CustomGridList(props: CustomGridListProps) {
   const [drinkSelected, setOpen] = useState(undefined)
 
@@ -133,7 +260,28 @@ export default function CustomGridList(props: CustomGridListProps) {
     update: 0
   }
   const [selectedDrinks, setSelectedDrinks] = useState<ISelectDrinks>(initSelectedDrinksState)
+
+  // Get localState
+
+  const initDrankDrinksState: IDrankDrinks = {
+    drankDrinks: [],
+    update: 1
+  }
+  const [drankDrinks, setDrankDrinks] = useState<IDrankDrinks>(initDrankDrinksState)
+
   const [openSelectedModal, setOpenSelectedModal] = useState(false)
+
+  useEffect(function () {
+    const ddd = localStorage.getItem('drankDrinks')
+
+    const iiii: IDrankDrinks = {
+      drankDrinks: ddd ? JSON.parse(ddd) : [],
+      update: 0
+    }
+    setDrankDrinks(iiii)
+  }, [])
+
+  const reeee = drankDrinks.drankDrinks.length === 0 ? null : CalculateBAC(drankDrinks, 0.55, 80)
 
   return (
     <>
@@ -143,9 +291,25 @@ export default function CustomGridList(props: CustomGridListProps) {
 
       <div role='list' className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
         {props.drinks.map((drink, i) => (
-          <ListItem key={i} drink={drink} selectedDrinks={selectedDrinks} setSelectedDrinks={setSelectedDrinks} />
+          <ListItem
+            key={i}
+            drink={drink}
+            selectedDrinks={selectedDrinks}
+            setSelectedDrinks={setSelectedDrinks}
+            drankDrinksState={drankDrinks}
+            setDrankDrinks={setDrankDrinks}
+          />
         ))}
       </div>
+
+      {JSON.stringify(drankDrinks)}
+
+      {reeee && (
+        <>
+          <div>{reeee.BAC}</div>
+          <LineChart data={reeee.BACData} />
+        </>
+      )}
     </>
   )
 }
